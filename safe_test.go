4e8b22ce1ba286e2f526dde9b70e01d469475502
lizongti <lzongti@gmail.com>
2022-11-08 14:26:11 +0800

    update test

--- safe_test.go
+++ safe_test.go
@@ -6,90 +6,90 @@ import (
 	"testing"
 	"time"
 
-	"github.com/frankban/quicktest"
+	. "github.com/frankban/quicktest"
 	"github.com/golibraries/safe"
 )
 
 func TestDo(t *testing.T) {
-	c := quicktest.New(t)
+	c := New(t)
 
-	c.Run("Do", func(c *quicktest.C) {
-		c.Run("nil", func(c *quicktest.C) {
+	c.Run("Do", func(c *C) {
+		c.Run("nil", func(c *C) {
 			err := safe.Do(nil)
-			c.Assert(err, quicktest.ErrorMatches, "panic: invalid function type: <nil>")
+			c.Assert(err, ErrorMatches, "panic: invalid function type: <nil>")
 		})
 
-		c.Run("func()", func(c *quicktest.C) {
+		c.Run("func()", func(c *C) {
 			err := safe.Do(func() {})
-			c.Assert(err, quicktest.IsNil)
+			c.Assert(err, IsNil)
 		})
 
-		c.Run("func() error", func(c *quicktest.C) {
+		c.Run("func() error", func(c *C) {
 			err := safe.Do(func() error { return nil })
-			c.Assert(err, quicktest.IsNil)
+			c.Assert(err, IsNil)
 		})
 	})
 }
 
 func TestDoWithContext(t *testing.T) {
-	c := quicktest.New(t)
+	c := New(t)
 
-	c.Run("DoWithContext", func(c *quicktest.C) {
-		c.Run("nil", func(c *quicktest.C) {
+	c.Run("DoWithContext", func(c *C) {
+		c.Run("nil", func(c *C) {
 			err := safe.DoWithContext(context.Background(), nil)
-			c.Assert(err, quicktest.ErrorMatches, "panic: invalid function type: <nil>")
+			c.Assert(err, ErrorMatches, "panic: invalid function type: <nil>")
 		})
 
-		c.Run("func()", func(c *quicktest.C) {
+		c.Run("func()", func(c *C) {
 			err := safe.DoWithContext(context.Background(), func() {})
-			c.Assert(err, quicktest.IsNil)
+			c.Assert(err, IsNil)
 		})
 
-		c.Run("func() error", func(c *quicktest.C) {
+		c.Run("func() error", func(c *C) {
 			err := safe.DoWithContext(context.Background(), func() error { return nil })
-			c.Assert(err, quicktest.IsNil)
+			c.Assert(err, IsNil)
 		})
 	})
 }
 
 func TestDoWithTimeout(t *testing.T) {
-	c := quicktest.New(t)
+	c := New(t)
 
-	c.Run("DoWithTimeout", func(c *quicktest.C) {
-		c.Run("nil", func(c *quicktest.C) {
+	c.Run("DoWithTimeout", func(c *C) {
+		c.Run("nil", func(c *C) {
 			err := safe.DoWithTimeout(time.Second, nil)
-			c.Assert(err, quicktest.ErrorMatches, "panic: invalid function type: <nil>")
+			c.Assert(err, ErrorMatches, "panic: invalid function type: <nil>")
 		})
 
-		c.Run("func()", func(c *quicktest.C) {
+		c.Run("func()", func(c *C) {
 			err := safe.DoWithTimeout(time.Second, func() {})
-			c.Assert(err, quicktest.IsNil)
+			c.Assert(err, IsNil)
 		})
 
-		c.Run("func() error", func(c *quicktest.C) {
+		c.Run("func() error", func(c *C) {
 			err := safe.DoWithTimeout(time.Second, func() error { return nil })
-			c.Assert(err, quicktest.IsNil)
+			c.Assert(err, IsNil)
 		})
 
-		c.Run("timeout", func(c *quicktest.C) {
+		c.Run("timeout", func(c *C) {
 			err := safe.DoWithTimeout(time.Nanosecond, func() {
 				time.Sleep(time.Second)
 			})
-			c.Assert(err, quicktest.ErrorMatches, "context deadline exceeded")
+			c.Assert(err, ErrorMatches, "context deadline exceeded")
 		})
 
-		c.Run("panic", func(c *quicktest.C) {
+		c.Run("panic", func(c *C) {
 			err := safe.DoWithTimeout(time.Second, func() {
 				panic("foo")
 			})
-			c.Assert(err, quicktest.ErrorMatches, "panic: foo")
+			c.Assert(err, ErrorMatches, "panic: foo")
 		})
 
-		c.Run("error", func(c *quicktest.C) {
+		c.Run("error", func(c *C) {
 			err := safe.DoWithTimeout(time.Second, func() error {
 				return errors.New("foo")
 			})
-			c.Assert(err, quicktest.ErrorMatches, "foo")
+			c.Assert(err, ErrorMatches, "foo")
 		})
 	})
 }
